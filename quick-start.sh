36cc5a06fb6683e3004c4b44c070aad8dc6d3af7
Jerome Su <suchuanxj@gmail.com>
2023-05-06 15:28:02 +0800

    shell脚本

--- quick-start.sh
+++ quick-start.sh
@@ -3,8 +3,7 @@ port=$2
 
 # install nginx
 yum install nginx
-sed -i e "s/~domain/$domain/" ../templates/server/nginx/nginx1.conf
-sed -i e "s/~port/$port/" ../templates/server/nginx/nginx.conf
+curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/nginx.conf | sed -e "s/~domain/jeromesu.com/" -e "s/~port/10053/" > /etc/nginx/conf/nginx.conf
 
 # ssl
 curl https://get.acme.sh | sh -s email=suchuanxj@gmail.com
@@ -16,10 +15,4 @@ acme.sh --install-cert -d $domain --key-file /etc/pki/nginx/private/server.key -
 # install v2ray
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
-curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/v2ray/conf.json > /usr/local/etc/v2ray/config.json
-sed -i e "s/~port/$port/" ../templates/server/v2ray/conf.json
-sed -i e "s/~id/$(uuidgen)/" ../templates/server/v2ray/conf.json
-
-
-firewall-cmd --zone=public --add-port=10053
-setenforce 0
+curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/v2ray/conf.json | sed -e "s/~port/$port/" -e "s/~id/$(uuidgen)/" > /usr/local/etc/v2ray/config.json
