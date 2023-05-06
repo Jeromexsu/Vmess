386ebe1354856a4be2169171a16ec7eb8eb5e65d
Jerome Su <suchuanxj@gmail.com>
2023-05-06 17:13:12 +0800

    script

--- quick-start.sh
+++ quick-start.sh
@@ -6,22 +6,19 @@ echo "installing nginx"
 yum -y install nginx 2>/dev/null >/dev/null
 (curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/nginx.conf  >/etc/nginx/nginx.conf) 2>/dev/null >/dev/null
 (curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/server80.conf | sed -e "s/~domain/$domain/g" >/etc/nginx/conf.d/server80.conf)
-(curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/server443.conf | sed -e "s/~domain/$domain/g" -e "s/~port/$port/" >/etc/nginx/conf.d/server443.conf)
 echo "conf for nginx: /etc/nginx/nginx.conf"
 
 # ssl
 (curl https://get.acme.sh | sh -s email=suchuanxj@gmail.com) 2>/dev/null >/dev/null
 mkdir /etc/pki/nginx/
 mkdir /etc/pki/nginx/private
-echo "tmp" > /etc/pki/nginx/server.crt
-echo "tmp" > "/etc/pki/nginx/private/server.key"
 
 echo "applying certificates"
 .acme.sh/acme.sh --issue -d $domain --nginx 2>/dev/null >/dev/null
-rm -f /etc/pki/nginx/server.crt
-rm -f /etc/pki/nginx/private/server.key
 echo "installing certificates"
 acme.sh --install-cert -d $domain --key-file /etc/pki/nginx/private/server.key --fullchain-file /etc/pki/nginx/server.crt 2>/dev/null >/dev/null
+
+(curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/server443.conf | sed -e "s/~domain/$domain/g" -e "s/~port/$port/" >/etc/nginx/conf.d/server443.conf)
 
 # install v2ray
 echo "installing v2ray"
