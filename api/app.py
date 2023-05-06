a1c52cbd9d4034a70634821fc24b3ae2f75bfedc
Jerome Su <suchuanxj@gmail.com>
2023-05-06 22:40:30 +0800

    client api

---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import Response
+
+app = Flask(__name__)
+def make_file(template_name,uuid):
+        placeholder = '~uuid'
+        with open('templates/%s' % template_name,'r') as clashx_file:
+            for line in clashx_file:
+                if placeholder in line: line = line.replace(placeholder,uuid)
+                yield line     
+
+def make_response(template_name,uuid):
+    response = Response(make_file(template_name,uuid),content_type="application/octet-stream")
+    response.headers['Content-Disposition'] = 'attachment; filename=%s' % template_name
+    return response
+
+@app.route("/clashx/<uuid>")
+def clashx_conf(uuid):
+    template_name = "clashx.yaml"
+    return make_response(template_name,uuid)
+
+@app.route("/shadowrocket/<uuid>")
+def shadowrocket_conf(uuid):
+    return clashx_conf(uuid)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5500)
