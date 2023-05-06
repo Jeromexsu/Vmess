e80b7fe0c61bfa13e60a1d4668ad74872b91e5e6
Jerome Su <suchuanxj@gmail.com>
2023-05-06 15:50:35 +0800

    shell script

--- quick-start.sh
+++ quick-start.sh
@@ -2,17 +2,25 @@ domain=$1
 port=$2
 
 # install nginx
-yum install nginx
-curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/nginx.conf | sed -e "s/~domain/jeromesu.com/" -e "s/~port/10053/" > /etc/nginx/conf/nginx.conf
+echo "installing nginx"
+yum install nginx 2>/dev/null >/dev/null
+curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/nginx/nginx.conf | sed -e "s/~domain/jeromesu.com/" -e "s/~port/10053/" >/etc/nginx/nginx.conf
+echo "conf for nginx: /etc/nginx/nginx.conf"
 
 # ssl
-curl https://get.acme.sh | sh -s email=suchuanxj@gmail.com
-acme.sh --issue -d $domain --nginx
+
+(curl https://get.acme.sh | sh -s email=suchuanxj@gmail.com) 2>/dev/null >/dev/null
+echo "applying certificates"
+acme.sh --issue -d $domain --nginx 2>/dev/null >/dev/null
 mkdir /etc/pki/nginx/
 mkdir /etc/pki/nginx/private
-acme.sh --install-cert -d $domain --key-file /etc/pki/nginx/private/server.key --fullchain-file /etc/pki/nginx/server.crt
+echo "installing certificates"
+acme.sh --install-cert -d $domain --key-file /etc/pki/nginx/private/server.key --fullchain-file /etc/pki/nginx/server.crt 2>/dev/null >/dev/null
 
 # install v2ray
-bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
-curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/v2ray/conf.json | sed -e "s/~port/$port/" -e "s/~id/$(uuidgen)/" > /usr/local/etc/v2ray/config.json
+echo "installing v2ray"
+(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | bash) 2>/dev/null >/dev/null
+(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh| bash) 2>/dev/null >/dev/null
+id=$(uuidgen)
+echo "uuid for test client: $id"
+(curl https://raw.githubusercontent.com/Jeromexsu/Vmess/main/templates/server/v2ray/conf.json | sed -e "s/~port/$port/" -e "s/~id/$id/" > /usr/local/etc/v2ray/config.json) 2>/dev/null >/dev/null
