149f67ceb120629563a87a04b6b96aafef6c1497
Jerome Su <suchuanxj@gmail.com>
2023-05-10 14:58:39 +0800

    update app.py

--- api/app.py
+++ api/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask import Response, stream_with_context
-import os
-
+import base64
 app = Flask(__name__)
 def make_file(template_name,uuid):
-        bytes = b""
+        string = ""
         placeholder = '~uuid'
-        with open('templates/%s' % template_name,'r') as clashx_file:
-            for line in clashx_file:
+        with open('templates/%s' % template_name,'r') as template_file:
+            for line in template_file:
                 if placeholder in line: line = line.replace(placeholder,uuid)
-                bytes += line.encode("utf-8") 
-        return bytes
+                string += line
+        return string
 
 def make_response(template_name,uuid):
     file_bytes = make_file(template_name,uuid)
@@ -25,8 +24,15 @@ def clashx_conf(uuid):
     return make_response(template_name,uuid)
 
 @app.route("/api/shadowrocket/<uuid>")
+@app.route("/api/v2ray/<uuid>")
 def shadowrocket_conf(uuid):
-    return clashx_conf(uuid)
+    template_name = "vmess.json"
+    file = make_file(template_name,uuid)
+    file_encoded = base64.b64encode(file.encode('utf-8')).decode('utf-8')
+    vmess_url = 'vmess://' + file_encoded
+    return base64.b64encode(vmess_url.encode('utf-8')).decode('utf-8')
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5500)
